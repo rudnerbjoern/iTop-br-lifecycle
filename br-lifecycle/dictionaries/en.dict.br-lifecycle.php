d9cc7b2ea0bbef291b8e86b2ea2736fd1072854d
Bjoern Rudner <rudnerbjoern@hotmail.com>
2025-06-12 14:26:27 +0200

    set compatibility to iTop 3.2

--- br-lifecycle/dictionaries/en.dict.br-lifecycle.php
+++ br-lifecycle/dictionaries/en.dict.br-lifecycle.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @copyright   Copyright (C) 2023-2024 Björn Rudner
+ * @copyright   Copyright (C) 2023-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-10
+ * @version     2025-06-12
  *
  * Localized data
  */
